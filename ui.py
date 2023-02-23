aee4c077cd8d7388286a88eda5bed7b383f1726a
Jeiojey <signofaries@gmail.com>
2023-02-23 18:53:26 +0300

    feat: add  search menu

--- ui.py
+++ ui.py
@@ -31,21 +31,21 @@ def notes_menu(number: str = '') -> int:
 
     print((f'{Style.BRIGHT + Fore.YELLOW}\nВыберите действие с записной книжкой (введите цифру от 1 до 4): \n{Style.RESET_ALL}'
           '1 - 📜 Просмотреть все записи \n'
-           '2 - 👀 Поиск записи по дате \n'
+           '2 - 👀 Поиск записей \n'
            '3 - ✍️ Добавить новую запись \n'
            '4 - ✈️ Завершить работу\n'
            f'{Style.RESET_ALL}'))
     return ch.check_menu(number, 5)
 
 
-# def input_search(number: str = '') -> str:
-#     '''
-#     Подменю для поиска контакта.
-#     '''
-#     print(e.emojize(f'{Style.BRIGHT + Fore.YELLOW}Выберите по какому параметру будет поиск (введите 1 или 2): {Style.RESET_ALL} \n'
-#                     '1 - :input_latin_lowercase: По имени \n'
-#                     '2 - :input_numbers: По номеру телефона'))
-#     return ch.check_phone_menu(number, 3)
+def input_search(number: str = '') -> int:
+    '''
+    Подменю для поиска записей.
+    '''
+    print(e.emojize(f'{Style.BRIGHT + Fore.YELLOW}Выберите по какому параметру будет поиск (введите 1 или 2): {Style.RESET_ALL} \n'
+                    '1 - 🔡 По тексту записи \n'
+                    '2 - 📆 По дате записи'))
+    return ch.check_menu(number, 3)
 
 
 def search_submenu(number: str = '') -> int:
@@ -54,19 +54,8 @@ def search_submenu(number: str = '') -> int:
     '''
     print((f'{Style.BRIGHT + Fore.YELLOW}:eyes:  Выберите действие для работы: {Style.RESET_ALL} \n'
            '1 - 🧹 Удалить запись \n'
-           '2 - ✏️Редактировать запись'))
+           '2 - ✏️ Редактировать запись'))
     return ch.check_menu(number, 3)
-
-
-def print_all(data_list: list) -> None:
-    '''
-    Вывод всех записей в консоль
-    '''
-    print((
-        f'{Style.BRIGHT + Fore.YELLOW}📜Ваши записи: {Style.RESET_ALL}'))
-    for line in data_list:
-        print(line)
-
 
 def print_note(contact_data: str = '') -> None:
     '''
@@ -101,11 +90,3 @@ def edit_data(note_data: str = '') -> str:
     print((
         f'{Style.BRIGHT + Fore.YELLOW}📇Введите новые данные: {Style.RESET_ALL}'))
     return ch.check_length(note_data)
-
-# def search(contact: str = '') -> None:
-#     '''
-#     Ввод в поисковую строку и поиск
-#     '''
-#     contact = str(input(e.emojize(
-#         f'{Style.BRIGHT + Fore.YELLOW}:writing_hand: Введите искомый контакт:{Style.RESET_ALL} '))).capitalize()
-#     return
